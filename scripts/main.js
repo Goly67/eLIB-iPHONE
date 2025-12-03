b6b19ccde3c02cba5a88c5f79be2b4bd62944e94
sammy <tufysooy@gmail.com>
2025-12-04 07:45:36 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -25,6 +25,35 @@ if ('serviceWorker' in navigator) {
 
 const topToast = document.getElementById('topToast');
 
+    (function () {
+      const ua = navigator.userAgent || navigator.vendor || window.opera;
+      const isIOS = /iPad|iPhone|iPod/.test(ua) && !window.MSStream;
+
+      if (!isIOS) {
+        document.body.innerHTML = `
+        <div class="overlay"></div>  
+<div class="content">
+  <div class="access-denied-card">
+    <img src="drawables/ios-logo.png" alt="iOS Logo" class="ios-logo">
+    <h1>Access Denied</h1>
+    <p>This website is only available on iOS devices, if you are using an android please download the app.</p>
+    <button onclick="window.location.href='https://www.apple.com/ios/'" class="learn-more-btn">
+      Learn More About iOS
+    </button>
+  </div>
+</div>
+        `;
+        throw new Error('Non-iOS device detected. Access denied.');
+      }
+    })();
+
+if (!token) {
+    alert('Unauthorized access. Please scan QR code first.');
+    window.location.href = 'scan.html';
+} else {
+    sessionStorage.setItem('qrSessionToken', token); // <-- save token
+} 
+
 function showTopToast(message) {
   topToast.textContent = message;
   topToast.style.display = "block";
@@ -282,3 +311,4 @@ if (btnNotif) {
     }
   });
 }
+
